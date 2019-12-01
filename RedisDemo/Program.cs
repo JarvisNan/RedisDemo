4446e9b3072d1c6c89b579fb8001d9e38238bec6
Jarvis <545444669@qq.com>
2019-12-01 21:54:14 +0800

    数据库连接实例

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace RedisDemo
 {
@@ -14,7 +15,14 @@ namespace RedisDemo
     {
         public static void Main(string[] args)
         {
+            var redis = ConnectionMultiplexer.Connect("122.152.205.186:6379,password=123456");
+            var db = redis.GetDatabase();
+
+            db.StringSet("name", "giao");
+            var name = db.StringGet("name");
+            Console.WriteLine(name);
             CreateWebHostBuilder(args).Build().Run();
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
